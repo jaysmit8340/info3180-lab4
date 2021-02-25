6e464b3ba3dff789213cf566c82ed3962b65eb2d
jaysmit8340 <jasonsmit95@gmail.com>
2021-02-24 17:02:48 -0800

    Made changes to files

--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ from app import app
 from flask import render_template, request, redirect, url_for, flash, session, abort
 from werkzeug.utils import secure_filename
 from app.forms import UploadForm
-
+from flask import send_from_directory
 
 ###
 # Routing for your application.
@@ -51,21 +51,23 @@ def get_uploaded_images():
     rootdir=os.getcwd()
     lst=[]
     print (rootdir)
-    for subdir, dirs, files in os.walk(rootdir + './uploads'):
+    for subdir, dirs, files in os.walk(rootdir + '/uploads'):
         for file in files:
-            lst.append(os.path.join(subdir, file).split('/')[-1])
+          lst.append(file)
+    lst.pop(0)
     return lst
 
-@app.route("/uploads/<filename>")
+@app.route('/upload/<filename>')
 def get_image(filename):
-     return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+    root_dir=os.getcwd()
+    return send_from_directory(os.path.join(root_dir,app.config['UPLOAD_FOLDER']),filename)
 
 @app.route("/files")
 def files():
     if not session.get('logged_in'):
          abort(401)
     img=get_uploaded_images()
-    return render_template('files.html', img=img)
+    return render_template("files.html", img=img)
 
 
 
