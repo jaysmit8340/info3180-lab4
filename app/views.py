78fa6a9cfdf4b90c557c32c0344309ecf0369012
jaysmit8340 <jasonsmit95@gmail.com>
2021-02-28 16:09:50 -0800

    Made changes to files

--- app/views.py
+++ app/views.py
@@ -24,7 +24,7 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Jason Smith")
 
 
 @app.route('/upload', methods=['POST', 'GET'])
